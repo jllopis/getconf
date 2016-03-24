500c2742873e7c6e1bf63b82fc897673178925dc
Joan Llopis <jllopisg@gmail.com>
2016-03-24 11:28:11 +0100

    added support for docker/libkv

---
+++ kvstore.go
@@ -0,0 +1,70 @@
+package getconf
+
+import (
+	"crypto/tls"
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/docker/libkv"
+	"github.com/docker/libkv/store"
+	"github.com/docker/libkv/store/consul"
+)
+
+type KVOptions struct {
+	Backend  string
+	URLs     []string
+	KVConfig *Config
+}
+
+// Config contains the options for a storage client
+type Config struct {
+	ClientTLS         *ClientTLSConfig
+	TLS               *tls.Config
+	ConnectionTimeout time.Duration
+	Bucket            string
+	PersistConnection bool
+}
+
+// ClientTLSConfig contains data for a Client TLS configuration in the form
+// the etcd client wants it.  Eventually we'll adapt it for ZK and Consul.
+type ClientTLSConfig struct {
+	CertFile   string
+	KeyFile    string
+	CACertFile string
+}
+
+func (gc *GetConf) EnableKVStore(opts *KVOptions) (*GetConf, error) {
+	switch strings.ToLower(opts.Backend) {
+	case "consul":
+		// Register consul store to libkv
+		consul.Register()
+		// Parse config
+		c := &store.Config{
+			TLS:               opts.KVConfig.TLS,
+			ConnectionTimeout: opts.KVConfig.ConnectionTimeout,
+			Bucket:            opts.KVConfig.Bucket,
+			PersistConnection: opts.KVConfig.PersistConnection,
+		}
+		if opts.KVConfig.ClientTLS != nil {
+			c.ClientTLS = &store.ClientTLSConfig{
+				CertFile:   opts.KVConfig.ClientTLS.CertFile,
+				KeyFile:    opts.KVConfig.ClientTLS.KeyFile,
+				CACertFile: opts.KVConfig.ClientTLS.CACertFile,
+			}
+		}
+		// Initialize a new store with consul
+		kv, err := libkv.NewStore(
+			store.CONSUL,
+			opts.URLs,
+			c,
+		)
+		if err != nil {
+			return gc, errors.New("cannot create store consul")
+		}
+		gc.KVStore = kv
+	default:
+		return gc, errors.New("unknown backend")
+	}
+	return gc, nil
+}
