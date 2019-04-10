14e43ba946c3fa04e303ccbc8d9026421ac5dd8f
Joan Llopis <jllopisg@gmail.com>
2019-04-10 17:38:52 +0200

    added string to date conversion from spf13/cast

--- getconf.go
+++ getconf.go
@@ -384,23 +384,59 @@ func getTypedValue(opt string, t reflect.Kind) interface{} {
 	case reflect.String:
 		return string(opt)
 	case reflect.Struct:
-		if t, err := time.Parse(time.RFC3339Nano, opt); err == nil {
+		if t, err := StringToDate(opt); err == nil {
 			return t
 		}
-		if t, err := time.Parse("2006-01-02T15:04:05", opt); err == nil {
-			return t
-		}
-		if t, err := time.Parse("2006-01-02 15:04:05", opt); err == nil {
-			return t
-		}
-		if t, err := time.Parse("2006-01-02", opt); err == nil {
-			return t
-		}
-
 		if sec, err := strconv.ParseInt(opt, 10, 64); err == nil {
 			return time.Unix(sec, 0)
 		}
 		return time.Time{}
 	}
 	return nil
+}
+
+// From https://github.com/spf13/cast/blob/master/caste.go
+// Copyright © 2014 Steve Francia <spf@spf13.com>.
+// StringToDate attempts to parse a string into a time.Time type using a
+// predefined list of formats.  If no suitable format is found, an error is
+// returned.
+func StringToDate(s string) (time.Time, error) {
+	return parseDateWith(s, []string{
+		time.RFC3339,
+		time.RFC3339Nano,
+		"2006-01-02T15:04:05", // iso8601 without timezone
+		time.RFC1123Z,
+		time.RFC1123,
+		time.RFC822Z,
+		time.RFC822,
+		time.RFC850,
+		time.ANSIC,
+		time.UnixDate,
+		time.RubyDate,
+		"2006-01-02 15:04:05.999999999 -0700 MST", // Time.String()
+		"2006-01-02",
+		"02 Jan 2006",
+		"2006-01-02T15:04:05-0700", // RFC3339 without timezone hh:mm colon
+		"2006-01-02 15:04:05 -07:00",
+		"2006-01-02 15:04:05 -0700",
+		"2006-01-02 15:04:05Z07:00", // RFC3339 without T
+		"2006-01-02 15:04:05Z0700",  // RFC3339 without T or timezone hh:mm colon
+		"2006-01-02 15:04:05",
+		time.Kitchen,
+		time.Stamp,
+		time.StampMilli,
+		time.StampMicro,
+		time.StampNano,
+	})
+}
+
+// From https://github.com/spf13/cast/blob/master/caste.go
+// Copyright © 2014 Steve Francia <spf@spf13.com>.
+func parseDateWith(s string, dates []string) (d time.Time, e error) {
+	for _, dateType := range dates {
+		if d, e = time.Parse(dateType, s); e == nil {
+			return
+		}
+	}
+	return d, fmt.Errorf("unable to parse date: %s", s)
 }
